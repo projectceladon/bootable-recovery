0d57e0dd7093b6f19520fc5e953bdb1118235e45
Ming Tan <ming.tan@intel.com>
2017-11-03 21:41:57 +0800

    Fix the segment fault of recovery.
    Copy some code from
    https://github.com/android-ia/bootable-recovery/commit/ad417391514874b803aff94b6f72361c674cbb6c
    
    Jira: None
    Test: None
    
    Signed-off-by: Ming Tan <ming.tan@intel.com>

--- minui/graphics_drm.cpp
+++ minui/graphics_drm.cpp
@@ -373,9 +373,41 @@ GRSurface* MinuiBackendDrm::Init() {
   return GRSurfaceDrms[0];
 }
 
+int g_flip_ongoing = 0;
+static void page_flip_complete(int,
+    unsigned int,
+    unsigned int,
+    unsigned int,
+    void *) {
+  printf("drm_flip_complete");
+  g_flip_ongoing = 0;
+}
+
 GRSurface* MinuiBackendDrm::Flip() {
-  int ret = drmModePageFlip(drm_fd, main_monitor_crtc->crtc_id,
-                            GRSurfaceDrms[current_buffer]->fb_id, 0, nullptr);
+  int ret;
+  drmEventContext evctx;
+  //check and wait for last flip complete
+  if (g_flip_ongoing) {
+    printf("drm_flip wait for last drm_flip complete");
+    usleep(20000);
+  }
+  g_flip_ongoing = 1;
+  ret = drmModePageFlip(drm_fd, main_monitor_crtc->crtc_id,
+                        GRSurfaceDrms[current_buffer]->fb_id,DRM_MODE_PAGE_FLIP_EVENT, NULL);
+  while (g_flip_ongoing) {
+    printf("drm_flip wait for drm_flip complete");
+    memset(&evctx, 0, sizeof evctx);
+    evctx.version = DRM_EVENT_CONTEXT_VERSION;
+    evctx.vblank_handler = NULL;
+    evctx.page_flip_handler = page_flip_complete;
+    ret = drmHandleEvent(drm_fd, &evctx);
+    if (ret != 0) {
+      printf("drmHandleEvent failed: %i\n", ret);
+      return NULL;
+    }
+    usleep(20000);
+  }
+
   if (ret < 0) {
     printf("drmModePageFlip failed ret=%d\n", ret);
     return nullptr;
